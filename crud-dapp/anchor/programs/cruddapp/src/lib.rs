99886431eb96b2e0c4eb658fa297d3dd7d2e745a
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-02 02:20:25 +0530

    feat: crud instructions

--- crud-dapp/anchor/programs/cruddapp/src/lib.rs
+++ crud-dapp/anchor/programs/cruddapp/src/lib.rs
@@ -22,6 +22,23 @@ pub mod cruddapp {
         journal_entry.message = message;
         Ok(())
     }
+
+    pub fn update_journal_entry(
+        ctx: Context<UpdateEntry>,
+        _title: String,
+        message: String,
+    ) -> Result<()> {
+        msg!("Journal Entry Updated");
+        msg!("Title: {}", _title);
+        msg!("Message: {}", message);
+        let journal_entry = &mut ctx.accounts.journal_entry;
+        journal_entry.message = message;
+        Ok(())
+    }
+
+    pub fn delete_journal_entry(_ctx: Context<DeleteEntry>, _title: String) -> Result<()> {
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -41,7 +58,40 @@ pub struct CreateEntry<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct UpdateEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    #[account(
+      mut,
+      realloc=8+JournalEntryState::INIT_SPACE,
+      realloc::zero=true,
+      realloc::payer=owner,
+      seeds=[title.as_bytes(),owner.key().as_ref()],
+      bump)]
+    pub journal_entry: Account<'info, JournalEntryState>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteEntry<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    #[account(
+      mut,
+      realloc=8+JournalEntryState::INIT_SPACE,
+      realloc::zero=true,
+      realloc::payer=owner,
+      seeds=[title.as_bytes(),owner.key().as_ref()],
+      bump)]
+    pub journal_entry: Account<'info, JournalEntryState>,
+}
+
 #[account]
+#[derive(InitSpace)]
 pub struct JournalEntryState {
     pub owner: Pubkey,
     #[max_len(32)]
