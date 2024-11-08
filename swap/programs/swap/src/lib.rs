5704f0c5862a7f310a1c962cb039ae2d4c120eec
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-08 19:52:55 +0530

    init:swap program

---
+++ swap/programs/swap/src/lib.rs
@@ -0,0 +1,21 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("5Srv4X1YJBAZGiS53BUVzgKYf8uQ5Le1Bawdrf7sZ6ZJ");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        initialize::handler(ctx)
+    }
+}
