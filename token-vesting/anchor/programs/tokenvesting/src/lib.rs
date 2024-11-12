16b793b4d76b7cb5b45ab643ba843acaf619e2f7
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-12 20:41:03 +0530

    feat:added company token vesting struct

--- token-vesting/anchor/programs/tokenvesting/src/lib.rs
+++ token-vesting/anchor/programs/tokenvesting/src/lib.rs
@@ -20,3 +20,28 @@ pub struct CreateVestingAccount<'info> {
     #[account(mut)]
     pub signer: Signer<'info>,
 }
+
+#[account]
+#[derive(InitSpace, Debug)]
+pub struct VestingAccount {
+    pub owner: Pubkey,
+    pub mint: Pubkey,
+    pub treasury_token_account: Pubkey,
+    #[max_len(50)]
+    pub company_name: String,
+    pub treasury_bump: u8,
+    pub bump: u8,
+}
+
+#[account]
+#[derive(InitSpace, Debug)]
+pub struct EmployeeAccount {
+    pub beneficiary: Pubkey,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub total_amount: i64,
+    pub total_withdrawal: i64,
+    pub cliff_time: i64,
+    pub vesting_account: Pubkey,
+    pub bump: u8,
+}
