849886080b6b6e02676337fc2b79892de5416694
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-11-16 19:52:37 +0530

    feat:Lottery init instruction

--- token-lottery/anchor/programs/tokenlottery/src/lib.rs
+++ token-lottery/anchor/programs/tokenlottery/src/lib.rs
@@ -1,11 +1,22 @@
 #![allow(clippy::result_large_err)]
-
 use anchor_lang::prelude::*;
-
-declare_id!("AsjZ3kWAUSQRNt2pZVeJkywhZ6gpLpHZmJjduPmKZDZZ");
+use anchor_lang::system_program;
+use anchor_spl::metadata::{
+    create_master_edition_v3, create_metadata_accounts_v3,
+    mpl_token_metadata::types::{CollectionDetails, Creator, DataV2},
+    set_and_verify_sized_collection_item, sign_metadata, CreateMasterEditionV3,
+    CreateMetadataAccountsV3, Metadata, MetadataAccount, SetAndVerifySizedCollectionItem,
+    SignMetadata,
+};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token_interface::{mint_to, Mint, MintTo, TokenAccount, TokenInterface},
+};
+use switchboard_on_demand::accounts::RandomnessAccountData;
+declare_id!("75QZtVPkNxku1hHt9p86rT5pjbkWw8YTsJiD5jZEseXg");
 
 #[program]
-pub mod basic {
+pub mod token_lottery {
     use super::*;
     pub fn Initialize_config(
         ctx: Context<Initialize>,
@@ -42,6 +53,47 @@ pub struct Initialize<'info> {
         bump
     )]
     pub token_lottery: Account<'info, TokenLottery>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeLottery<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        mint::decimals = 0,
+        mint::authority = collection_mint,
+        mint::freeze_authority = collection_mint,
+        seeds = [b"collection_mint".as_ref()],
+        bump,
+    )]
+    pub collection_mint: Box<InterfaceAccount<'info, Mint>>,
+
+    /// CHECK: This account will be initialized by the metaplex program
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
+
+    /// CHECK: This account will be initialized by the metaplex program
+    #[account(mut)]
+    pub master_edition: UncheckedAccount<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = payer,
+        seeds = [b"collection_token_account".as_ref()],
+        bump,
+        token::mint = collection_mint,
+        token::authority = collection_token_account
+    )]
+    pub collection_token_account: Box<InterfaceAccount<'info, TokenAccount>>,
+
+    pub token_program: Interface<'info, TokenInterface>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+    pub token_metadata_program: Program<'info, Metadata>,
+    pub rent: Sysvar<'info, Rent>,
 }
 
 #[account]
--- token-lottery/anchor/tests/tokenlottery.spec.ts
+++ token-lottery/anchor/tests/tokenlottery.spec.ts
@@ -1,76 +1,21 @@
-import * as anchor from '@coral-xyz/anchor'
-import {Program} from '@coral-xyz/anchor'
-import {Keypair} from '@solana/web3.js'
-import {Tokenlottery} from '../target/types/tokenlottery'
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Keypair } from "@solana/web3.js";
+import { TokenLottery } from "../target/types/token_lottery";
 
-describe('tokenlottery', () => {
-  // Configure the client to use the local cluster.
-  const provider = anchor.AnchorProvider.env()
-  anchor.setProvider(provider)
-  const payer = provider.wallet as anchor.Wallet
+describe("tokenlottery", () => {
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
+  const program = anchor.workspace.Tokenlottery as Program<TokenLottery>;
 
-  const program = anchor.workspace.Tokenlottery as Program<Tokenlottery>
-
-  const tokenlotteryKeypair = Keypair.generate()
-
-  it('Initialize Tokenlottery', async () => {
+  it("Initialize Tokenlottery", async () => {
     await program.methods
-      .initialize()
-      .accounts({
-        tokenlottery: tokenlotteryKeypair.publicKey,
-        payer: payer.publicKey,
-      })
-      .signers([tokenlotteryKeypair])
-      .rpc()
-
-    const currentCount = await program.account.tokenlottery.fetch(tokenlotteryKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(0)
-  })
-
-  it('Increment Tokenlottery', async () => {
-    await program.methods.increment().accounts({ tokenlottery: tokenlotteryKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.tokenlottery.fetch(tokenlotteryKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Increment Tokenlottery Again', async () => {
-    await program.methods.increment().accounts({ tokenlottery: tokenlotteryKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.tokenlottery.fetch(tokenlotteryKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(2)
-  })
-
-  it('Decrement Tokenlottery', async () => {
-    await program.methods.decrement().accounts({ tokenlottery: tokenlotteryKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.tokenlottery.fetch(tokenlotteryKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(1)
-  })
-
-  it('Set tokenlottery value', async () => {
-    await program.methods.set(42).accounts({ tokenlottery: tokenlotteryKeypair.publicKey }).rpc()
-
-    const currentCount = await program.account.tokenlottery.fetch(tokenlotteryKeypair.publicKey)
-
-    expect(currentCount.count).toEqual(42)
-  })
-
-  it('Set close the tokenlottery account', async () => {
-    await program.methods
-      .close()
-      .accounts({
-        payer: payer.publicKey,
-        tokenlottery: tokenlotteryKeypair.publicKey,
-      })
-      .rpc()
-
-    // The account should no longer exist, returning null.
-    const userAccount = await program.account.tokenlottery.fetchNullable(tokenlotteryKeypair.publicKey)
-    expect(userAccount).toBeNull()
-  })
-})
+      .initializeConfig(
+        new anchor.BN(0),
+        new anchor.BN(19999999999),
+        new anchor.BN(1000)
+      )
+      .rpc();
+  });
+});
